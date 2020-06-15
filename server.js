c82769f29552a70d6e8aa68df69b4efb5316716b
Wojtek Nowak <billnewmann@gmail.com>
2020-06-15 21:07:43 +0100

    Add 404 error endpoint

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get('/history', (req, res) => {
   res.show('history.html');
 });
 
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
