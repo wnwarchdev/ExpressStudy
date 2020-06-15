0496b62a30759b3404f6f6aa4f60df4f67b79f91
Wojtek Nowak <billnewmann@gmail.com>
2020-06-15 20:54:55 +0100

    Fix endpoints with use of middleware

--- server.js
+++ server.js
@@ -3,24 +3,31 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/contact.html'));
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/info.html'));
+  res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/history.html'));
+  res.show('history.html');
 });
 
 app.listen(8000, () => {
