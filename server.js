4cc5e390041b60e810939291d7a6514a69aa3e64
Wojtek Nowak <billnewmann@gmail.com>
2020-06-16 23:19:03 +0100

    Modified as per excercise directions

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use('/user/settings', (req, res, next) => {
+  res.send('Go away!');
+});
+
+app.use('/user/panel', (req, res, next) => {
+  res.send('Go home!');
+});
+
 app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/home', (req, res) => {
   res.show('index.html');
 });
 
@@ -20,20 +32,8 @@ app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
-app.get('/contact', (req, res) => {
-  res.show('contact.html');
-});
-
-app.get('/info', (req, res) => {
-  res.show('info.html');
-});
-
-app.get('/history', (req, res, next) => {
-  res.show('history.html');
-});
-
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).show('error.html');
 })
 
 app.get('/style.css', (req, res) => {
@@ -43,6 +43,7 @@ app.get('/style.css', (req, res) => {
 app.get('/test.jpg', (req, res) => {
   res.sendFile('test.jpg');
 });
+
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
